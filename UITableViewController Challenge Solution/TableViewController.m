a62e321f7fb35388ea0beb5c276d88b6c493bd51
jpall <juri.pall@epost-dev.de>
2015-03-03 22:58:58 +0100

    solve extra credit of the challenge

--- UITableViewController Challenge Solution/TableViewController.m
+++ UITableViewController Challenge Solution/TableViewController.m
@@ -58,10 +58,13 @@
     // Configure the cell...
     if (indexPath.section == 0) {
         cell.textLabel.text = @"I am in section 0";
+        cell.backgroundColor = [UIColor redColor];
     } else if (indexPath.section == 1) {
         cell.textLabel.text = @"another section";
+        cell.backgroundColor = [UIColor blueColor];
     } else {
         cell.textLabel.text = [NSString stringWithFormat:@"cell is in raw: %i", indexPath.row];
+        cell.backgroundColor = [UIColor yellowColor];
     }
     
     return cell;
